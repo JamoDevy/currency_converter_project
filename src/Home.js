9c6b5f801e5f0d81ba45bf90b32668894cfaa7f5
JamoDevy <devinlclarke@gmail.com>
2023-03-28 21:37:38 -0400

    Added forms and button

---
+++ src/Home.js
@@ -0,0 +1,50 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+
+class CurrencyConverter extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            base: '',
+            rates: '',
+            loading: true,
+        }
+    }
+
+
+render () {
+    const {base, rates, loading} = this.state;
+
+    return(
+        <React.Fragment>
+            <div className="container bg-light">
+                <div className="row">
+                    <div className="col-4">
+                <form className="p-3 form-inline form-inline-sm bg-light dropdown-content">
+                    <label>Amount</label><select class="form-control"></select>
+                </form>
+                </div>
+
+                <div className="col-4">
+                <form className="p-3 form-inline form-inline-sm bg-light dropdown-content">                   
+                    <label>From Rate</label><select class="form-control"></select>
+                </form>               
+                </div>
+
+                <div className="col-4">
+                <form className="p-3 form-inline form-inline-sm bg-light dropdown-content">                    
+                    <label>To Rate</label><select class="form-control"></select>
+                </form>               
+                </div>
+                <div className="converter-submit">
+                    <button className="submit-conversion">Convert</button>
+                </div>
+             </div>
+           </div>
+        </React.Fragment>
+    )
+  }
+}
+
+export default CurrencyConverter;
