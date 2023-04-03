a7f109205eb9ef91311cb97184aabc6847e0c8e2
JamoDevy <devinlclarke@gmail.com>
2023-04-02 20:13:27 -0400

    setting up currencies

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
-import { render } from "@testing-library/react";
+
 import React from "react";
+import { render } from "@testing-library/react";
+import currencies from './utils/currencies';
+import { checkStatus, json } from './utils/fetch'; 
 
 
 class CurrencyConverter extends React.Component {
